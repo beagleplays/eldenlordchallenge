8fba59b366d5de12937f48b5ee11a91b9baf01d0
beagleplays <thebeagleplays@gmail.com>
2024-11-02 12:17:21 +0100

    re build

--- docs/assets/index-C-v_Kbiz.js
+++ docs/assets/index-C-v_Kbiz.js
@@ -1,5 +1,1 @@
-<<<<<<<< HEAD:docs/assets/index-C-v_Kbiz.js
 import{d as e,c as n,r,o as t}from"./index-DZ1_MlcZ.js";const _=e({__name:"index",setup(c){return(s,a)=>{const o=r("HelloWorld");return t(),n(o)}}});export{_ as default};
-========
-import{d as e,c as n,r,o as t}from"./index-DvfpeIkf.js";const _=e({__name:"index",setup(c){return(s,a)=>{const o=r("HelloWorld");return t(),n(o)}}});export{_ as default};
->>>>>>>> e505253866aa477b37e2fb3ab3609b622cdf0408:docs/assets/index-CMlUXlds.js
